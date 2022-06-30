25e6d7c981f7d6f7ec409bc856fe0d3cb09b7ac6
danilo <danslvalves@hotmail.com>
2022-06-30 09:27:17 -0300

    Atividade modulos

---
+++ main.mjs
@@ -0,0 +1,7 @@
+import { mostraCidade, mostraIdade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Danilo', 23));
+
+console.log(mostraCidade('Danilo', 'Brasília'));
+
+console.log(mostraHobby('Danilo', 'Assitir futebol'));
